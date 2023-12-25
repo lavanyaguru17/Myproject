06637b5915a030d691ef8c5294ebef191a74b7b9
lavanyaguru17 <109407476+lavanyaguru17@users.noreply.github.com>
2023-12-25 06:22:26 +0530

    Update and rename print a sentence.java to add two integers.java

---
+++ JAVA/add two integers.java
@@ -0,0 +1,23 @@
+//compile-time
+public class sample{
+    public static void main(String[] args){
+        int first=2;
+        int second=3;
+        int sum=first+second;
+        System.out.println("Sum is: "+sum);
+    }
+}
+//run-time
+/*import java.util.Scanner;
+public class sample{
+    public static void main(String args[]){
+        System.out.println("Enter First number: ");
+        Scanner f=new Scanner(System.in);
+        int firstno=f.nextInt();
+        System.out.println("Enter Second number: ");
+        Scanner s=new Scanner(System.in);
+        int secondno=s.nextInt();
+        int sum=firstno+secondno;
+        System.out.println("Sum is: "+sum);
+    }
+}*/
